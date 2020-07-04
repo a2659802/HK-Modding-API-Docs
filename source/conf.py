5cd778b3f425a37684ea286910b56aa7961c26e6
SalehAce1 <seyedmohammadghaem@gmail.com>
2020-07-03 17:43:18 -0700

    Fixed issue where some sections were not appearing

--- source/conf.py
+++ source/conf.py
@@ -162,3 +162,9 @@ texinfo_documents = [
      author, 'HollowKnightModdingApi', 'One line description of project.',
      'Miscellaneous'),
 ]
+
+from sphinx.highlighting import lexers
+from pygments.lexers.web import PhpLexer
+lexers['php'] = PhpLexer(startinline=True, linenos=1)
+lexers['php-annotations'] = PhpLexer(startinline=True, linenos=1)
+primary_domain = 'php'
